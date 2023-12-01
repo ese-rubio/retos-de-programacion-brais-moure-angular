580108899f3ee36c236e731590de0c37f5813cda
ese-rubio <rubioser@hotmail.com>
2023-12-01 12:28:54 +0100

    Creating structure for the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Reto33Component } from './retos/reto-33/reto-33.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Reto33Component
   ],
   imports: [
     BrowserModule,
